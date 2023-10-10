a24eec0f02facd70ed7531e0441d6c4a17ad4117
Abdou-salam <dialloabdousalam20@gmail.com>
2023-10-10 09:25:48 +0200

    atelier2

--- atelier2/src/exercice1/Employe.java
+++ atelier2/src/exercice1/Employe.java
@@ -2,17 +2,17 @@ package exercice1;
 import java.util.*;
 
 public class Employe extends Personne{
-	private  float salaire;
+	private  double salaire;
 	private GregorianCalendar dateEmbauche;
-	private GregorianCalendar dateAujourdHui = new GregorianCalendar();
 	
-	private Employe(String nom,String prenom,GregorianCalendar dateNaissance,Adresse adresse,float salaire,GregorianCalendar dateEmbauche) {
+	
+	private Employe(String nom,String prenom,GregorianCalendar dateNaissance,Adresse adresse,double salaire,GregorianCalendar dateEmbauche) {
 		super(nom,prenom,dateNaissance,adresse);
 		this.salaire = salaire;
 		this.dateEmbauche = dateEmbauche;
 	}
 	
-	private Employe (String nom,String prenom,int j,int m,int a,int numero,String rue,String code_postal,String ville,float salaire,GregorianCalendar dateEmbauche) {
+	private Employe (String nom,String prenom,int j,int m,int a,int numero,String rue,String code_postal,String ville,double salaire,GregorianCalendar dateEmbauche) {
 		this(nom,prenom,new GregorianCalendar(a,m,j),new Adresse(numero,rue,code_postal,ville),salaire,dateEmbauche);
 	}
 	
@@ -21,6 +21,7 @@ public class Employe extends Personne{
 	}
 	
 	public Employe createEmploye() {
+		GregorianCalendar dateAujourdHui = new GregorianCalendar();
 		int age = (dateAujourdHui.get(dateAujourdHui.YEAR) - getDateNaissance().get(getDateNaissance().YEAR));
 		Employe e;
 		if (age > 16 && age < 65) {
@@ -38,6 +39,7 @@ public class Employe extends Personne{
 	}
 	
 	public int calculAnnuite(Employe e) {
+		GregorianCalendar dateAujourdHui = new GregorianCalendar();
 		return dateAujourdHui.get(dateAujourdHui.YEAR) -  e.getDateEmbauche().get(e.getDateEmbauche().YEAR);
 	}
 	
--- atelier2/src/exercice1/Manager.java
+++ atelier2/src/exercice1/Manager.java
@@ -3,20 +3,32 @@ package exercice1;
 import java.util.GregorianCalendar;
 
 public class Manager extends Personne{
-	private float salaire;
+	private double salaire;
 	private GregorianCalendar dateEmbauche;
+	private Secretaire secretaireManager;
 
-	private Manager(String nom,String prenom,GregorianCalendar dateNaissance,Adresse adresse,float salaire,GregorianCalendar dateEmbauche) {
+	private Manager(String nom,String prenom,GregorianCalendar dateNaissance,Adresse adresse,float salaire,GregorianCalendar dateEmbauche,Secretaire secretaireManager ) {
 		super(nom,prenom,dateNaissance,adresse);
 		this.salaire = salaire;
 		this.dateEmbauche = dateEmbauche;
+		this .secretaireManager = secretaireManager;
 	}
 	
-	private Manager (String nom,String prenom,int j,int m,int a,int numero,String rue,String code_postal,String ville,float salaire,GregorianCalendar dateEmbauche) {
-		this(nom,prenom,new GregorianCalendar(a,m,j),new Adresse(numero,rue,code_postal,ville),salaire,dateEmbauche);
+	private Manager (String nom,String prenom,int j,int m,int a,int numero,String rue,String code_postal,String ville,float salaire,GregorianCalendar dateEmbauche,Secretaire secretaireManager) {
+		this(nom,prenom,new GregorianCalendar(a,m,j),new Adresse(numero,rue,code_postal,ville),salaire,dateEmbauche,secretaireManager);
 	}
 	
-
+	public void augmenterLeSalaire (int pourcentage) {
+		GregorianCalendar dateAujourdHui = new GregorianCalendar();
+		int anciennete = dateAujourdHui.get(dateAujourdHui.YEAR) -  dateEmbauche.get(dateEmbauche.YEAR);
+		if (pourcentage > 0) {
+			salaire = ( salaire * pourcentage / 100 + anciennete * 0.5/100);
+		}
+	}
+	
+	public void setSecretaireManager(Secretaire secretaireManager) {
+		this.secretaireManager =secretaireManager;
+	}
 	
 
 }
